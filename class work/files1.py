69383643259464f906e3c6dc3170a51b2e6b9e66
Ben <90052277+benedicto7@users.noreply.github.com>
2022-04-25 14:31:40 -0400

    Delete class work directory

--- class work/files1.py
+++
@@ -1,5 +0,0 @@
-file = open('lines.txt', 'w')
-for i in range(0, 100):
-    file.write('Lines #' + str(i) + '\n')
-file.close()
-
--- class work/files2.py
+++
@@ -1,7 +0,0 @@
-afile.write("new line\n")
-afile = open("out.txt", "a")
-afile.write("new line\n")
-afile.write(2.0)
-afile.write("2.0")
-afile.close()
-afile.write("new line\n")
